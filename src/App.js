46a8210c3c5aae722f41daa4ebed5c9abddcd373
aezed <aezedr@gmail.com>
2018-10-21 15:28:04 -0700

    Add placeholder pages and basic navigation, remove default styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import Router from './Router';
+import Navigation from './Navigation';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>My Shopping Cart</h1>
+      <div>
+        <Navigation />
+        <Router />
       </div>
     );
   }
---
+++ src/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = (props) => (
+  <nav>
+    <ul>
+      <li><NavLink to='/'>Home</NavLink></li>
+      <li><NavLink to='/cart'>Cart</NavLink></li>
+    </ul>
+  </nav>
+);
+
+export default Navigation;

d5b37996c07c49dddf134bc8e7766ae55f530dc0
aezed <aezedr@gmail.com>
2018-10-28 19:00:17 -0700

    Add Stripe Checkout to cart

---
+++ src/features/cart/checkout.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout';
+
+import STRIPE_PUBLISHABLE from './constants/stripe';
+import PAYMENT_SERVER_URL from './constants/server';
+
+const CURRENCY = 'USD';
+
+const fromDollarstoCents = amount => amount * 100;
+
+const successPayment = data => {
+  alert('Payment Successful');
+};
+
+const errorPayment = data => {
+  alert('Payment Error');
+};
+
+const onToken = amount => token =>
+  axios.post(PAYMENT_SERVER_URL,
+    {
+      source: token.id,
+      currency: CURRENCY,
+      amount: fromDollarstoCents(amount)
+    })
+    .then(successPayment)
+    .catch(errorPayment);
+
+const Checkout = ({ amount }) =>
+  <StripeCheckout
+    amount={fromDollarstoCents(amount)}
+    token={onToken(amount)}
+    currency={CURRENCY}
+    stripeKey={STRIPE_PUBLISHABLE}
+  />
+
+export default Checkout;
--- src/features/cart/index.js
+++ src/features/cart/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Checkout from './checkout';
 
 const sort = items => items.sort((a, b) => a.id < b.id);
 
@@ -15,58 +16,61 @@ const Cart = (props) => {
     });
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Item</th>
-          <th>Quantity</th>
-          <th>Price</th>
-          <th>Subtotal</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          sort(props.cart).map((item, key) => {
-            const subTotal = (item.price * item.quantity).toLocaleString("en-US",
-              {
-                style: "currency",
-                currency: "USD",
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2
-              });
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Subtotal</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            sort(props.cart).map((item, key) => {
+              const subTotal = (item.price * item.quantity).toLocaleString("en-US",
+                {
+                  style: "currency",
+                  currency: "USD",
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2
+                });
 
-            return (
-              <tr key={key}>
-              <td>{item.name}</td>
-              <td>{item.quantity}</td>
-                <td>{item.price.toLocaleString("en-US",
-                  {
-                    style: "currency",
-                    currency: "USD",
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2
-                  })}</td>
-              <td>{subTotal}</td>
-              <td>
-                <button onClick={() => props.addToCart(item)}>+</button>
-              </td>
-              <td>
-                <button onClick={() => props.removeFromCart(item)}>-</button>
-              </td>
-              <td>
-                <button onClick={() => props.removeAllFromCart(item)}>Remove All</button>
-              </td>
-            </tr>
-            );
-          })
-        }
-        <tr>
-          <td>
-            <h4>Total: {total}</h4>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+              return (
+                <tr key={key}>
+                  <td>{item.name}</td>
+                  <td>{item.quantity}</td>
+                  <td>{item.price.toLocaleString("en-US",
+                      {
+                        style: "currency",
+                        currency: "USD",
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2
+                      })}</td>
+                  <td>{subTotal}</td>
+                  <td>
+                    <button onClick={() => props.addToCart(item)}>+</button>
+                  </td>
+                  <td>
+                    <button onClick={() => props.removeFromCart(item)}>-</button>
+                  </td>
+                  <td>
+                    <button onClick={() => props.removeAllFromCart(item)}>Remove All</button>
+                  </td>
+                </tr>
+              );
+            })
+          }
+          <tr>
+            <td>
+              <h4>Total: {total}</h4>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Checkout amount={total} />
+    </div>
   )
 }
 

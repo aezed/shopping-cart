42eab8666866a38ee17372a7297fea1b638f43ea
Aezed Raza <aezedr@gmail.com>
2018-11-07 19:40:18 -0600

    Refactor purchase confirmation

--- src/features/cart/checkout.js
+++ src/features/cart/checkout.js
@@ -9,42 +9,49 @@ const CURRENCY = 'USD';
 
 const fromDollarstoCents = amount => parseInt(amount * 100);
 
-const onToken = amount => token =>
-  axios.post(PAYMENT_SERVER_URL,
+export default class Checkout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {success: false, data: { amount: null, transactionID: null}};
+    this.successPayment = this.successPayment.bind(this);
+    this.errorPayment = this.errorPayment.bind(this);
+  }
+
+  successPayment(data) {
+    const amount = (data.data.success.amount / 100).toFixed(2);
+    const transactionID = data.data.success.id;
+    this.setState({success: true, data: { amount, transactionID }});
+  }
+
+  errorPayment(data) {
+    this.setState({success: false, data: data.data});
+    alert('Payment Error');
+  }
+
+  onToken = amount => token => axios.post(PAYMENT_SERVER_URL,
     {
       source: token.id,
       currency: CURRENCY,
       amount: fromDollarstoCents(amount)
     })
-    .then(Checkout.successPayment)
-    .catch(Checkout.errorPayment);
-
-export default class Checkout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {success: false, data: null}
-  }
-
-  successPayment(data) {
-    this.setState({success:true, data});
-    alert(data);
-  }
-
-  errorPayment(data) {
-    alert('Payment Error');
-    console.log(data);
-  }
+    .then(this.successPayment)
+    .catch(this.errorPayment);
 
   render() {
     return (
       <div>
         <StripeCheckout
           amount={fromDollarstoCents(this.props.amount)}
-          token={onToken(this.props.amount)}
+          token={this.onToken(this.props.amount)}
           currency={CURRENCY}
           stripeKey={STRIPE_PUBLISHABLE}
         />
-        {this.state.success === true && <div>Success</div>}
+        {this.state.success
+          ? <div style={{fontWeight: 'bold'}}>
+              <br />
+              {`Purchase Confirmed! Amount: $${this.state.data.amount}, Transaction ID: ${this.state.data.transactionID}`}
+            </div>
+          : null}
       </div>
     );
   }
